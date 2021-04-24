7bded1cb617983dfed5afcfa9c86c633c93215b1
Jun Yu (AMD) <shockzinfinity@gmail.com>
2021-04-24 14:12:24 +0900

    초기 커밋.

---
+++ Startup.cs
@@ -0,0 +1,89 @@
+using BlazorTemplate.Areas.Identity;
+using BlazorTemplate.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTemplate
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<ApplicationDbContext>(options =>
+          options.UseSqlServer(
+              Configuration.GetConnectionString("DefaultConnection")));
+      services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+          .AddEntityFrameworkStores<ApplicationDbContext>();
+      services.AddControllersWithViews(); // MVC way
+      services.AddRazorPages();
+      services.AddServerSideBlazor();
+      services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+      services.AddDatabaseDeveloperPageExceptionFilter();
+      services.AddSingleton<WeatherForecastService>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseMigrationsEndPoint();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        // MVC way
+        endpoints.MapControllerRoute(
+                  name: "default",
+                  pattern: "{controller=Home}/{action=Index}/{id?}");
+        endpoints.MapRazorPages();
+        endpoints.MapBlazorHub();
+        endpoints.MapFallbackToPage("/_Host");
+
+        // 루트 페이지 로딩하자마 특정 URL 로 이동하고자 할때
+        endpoints.MapGet("/", context =>
+        {
+          context.Response.Redirect("/Home"); // MVC Home 기본 페이지로 이동
+          return Task.CompletedTask;
+        });
+      });
+    }
+  }
+}
